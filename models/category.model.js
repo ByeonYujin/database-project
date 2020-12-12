4096067f85f4f279ad4f7a0ed8f7bc46b3a66579
Weisstrieb <weisstrieb@gmail.com>
2020-12-12 14:08:01 +0900

    Category 모델 추가

---
+++ models/category.model.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+
+const name = "category";
+const schema = {
+    // 카테고리 ID
+    catId: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+
+    // 카테고리명
+    name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+        unique: true
+    }
+}
+const opts = {
+    underscored: false,
+    timestamps: false
+}
+
+module.exports = (sequelize) => {
+    const category = sequelize.define(name, schema, opts)
+    category.hasOne(category, { as: "parent" })
+
+    return category
+};
